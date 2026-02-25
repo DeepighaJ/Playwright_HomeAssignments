a3aeb1b35953dc22435ffd247767f5dffa3c2686
DeepighaJ <jmdeepigha@gmail.com>
2026-02-24 17:42:27 -0800

    Create alertFrame.spec.ts
    w3 schools try it alert

---
+++ Week4/alertFrame.spec.ts
@@ -0,0 +1,41 @@
+import test, { expect } from '@playwright/test';
+
+test("Handle Alert thats inside a Frame",async({page})=>{
+
+    await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm")
+    let text = "Hi"
+    //Event listener
+    await page.on("dialog",alert=>{
+        const type = alert.type()
+
+        const defVal= alert.defaultValue();
+
+        const message = alert.message();
+
+        if(type==='alert')
+        {
+            alert.accept()
+        }
+        else if(type==='confirm')
+        {
+            alert.accept();
+
+        }
+        else if (type==='prompt')
+        {
+            alert.accept(text)
+        }
+
+
+    })
+
+    let expectedText='You pressed OK!'
+
+    await page.frameLocator('#iframeResult').locator("//button[text()='Try it']").click(); //clicking on Try it button
+
+    let actualText = await page.frameLocator("#iframeResult").locator("//p[@id='demo']").textContent(); //retrieving text 
+    
+    //assert the retrieved text
+    expect.soft(actualText).toEqual(expectedText);
+
+})
